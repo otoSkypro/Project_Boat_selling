586e06be31c1fc378f6e39363eb6cba7aaa5806d
Семья <oc83@mail.ru>
2024-03-28 15:23:36 +0300

    hello

--- boat/urls.py
+++ boat/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from boat.apps import BoatConfig
 from boat.views import BoatListView, BoatDetailView
+from order.views import OrderCreateView
 
 app_name = BoatConfig.name
 
 urlpatterns = [
     path('', BoatListView.as_view(), name='boat_list'),
     path('<int:pk>/', BoatDetailView.as_view(), name='boat_view'),
+    path('create/<int:pk>', OrderCreateView.as_view(), name='create'),
 ]
--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
+from boat.views import BoatListView, BoatDetailView
 from order.views import OrderCreateView
 from order.apps import OrderConfig
 
 app_name = OrderConfig.name
 
 urlpatterns = [
-    path('create/<int:pk>', OrderCreateView.as_view(), name='create')
+    path('create/<int:pk>', OrderCreateView.as_view(), name='create'),
 ]
